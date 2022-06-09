fa38b5dd6e05d6cc54aa120f2a6629f7c0ccdf3e
Jonathan Washington <jonathan.n.washington@gmail.com>
2022-06-08 22:53:32 -0400

    updated script I forgot to commit on May 11

--- dev/count_stems.sh
+++ dev/count_stems.sh
@@ -12,20 +12,20 @@ lexcfilein="../apertium-$LG.$LG.lexc"
 lexcfile="/tmp/apertium-$LG.$LG.lexc"
 grep -v "^\!" $lexcfilein > $lexcfile
 
-totalcount=$(grep -Eo ":\w+.*;" $lexcfile | grep -v "[<>]" | sort | uniq | wc -l)
+totalcount=$(grep -Eo ":[[:alnum:][:space:]%-\{\}]+.*;" $lexcfile | grep -v "[<>]" | sort | uniq | wc -l)
 #lexc=$(hfst-lexc $lexcfile -o /dev/null 2>&1)
 #Nouns=$(grep -E ":\w+\s+N[0-9].*;" $lexcfile | grep -v "[<>]" | wc -l)
 Nouns=$(grep -Eo ":[[:alnum:][:space:]%-\{\}]+\s+N(-.*|[0-9]|[0-9]-.*)?\s*;" $lexcfile | grep -v "[<>]" | sort | uniq | wc -l)
-Adjectives=$(grep -Eo ":\w+\s+A[0-9].*;" $lexcfile | grep -v "[<>]" | sort | uniq | wc -l)
-Verbs=$(grep -Eo ":\w+\s+V-.*;" $lexcfile | grep -v "[<>]" | sort | uniq | wc -l)
-ProperNouns=$(grep -Eo ":\w+\s+NP[0-9]*-.*;" $lexcfile | grep -v "[<>]" | sort | uniq | wc -l)
-Adverbs=$(grep -Eo ":\w+\s+ADV[0-9]*-*.*;" $lexcfile | grep -v "[<>]" | sort | uniq | wc -l)
-Dets=$(grep -Eo ":\w+\s+DET[0-9]*-*.*;" $lexcfile | grep -v "[<>]" | sort | uniq | wc -l)
-Conjunctions=$(grep -Eo ":\w+\s+C[CAS].*;" $lexcfile | grep -v "[<>]" | sort | uniq | wc -l)
-Postpositions=$(grep -Eo ":\w+\s+POST[0-9]*-*.*;" $lexcfile | grep -v "[<>]" | sort | uniq | wc -l)
-Ijs=$(grep -Eo ":\w+\s+INTERJ[0-9]*-*.*;" $lexcfile | grep -v "[<>]" | sort | uniq | wc -l)
-Numerals=$(grep -Eo ":\w+\s+NUM[0-9]*-*.*;" $lexcfile | grep -v "[<>]" | sort | uniq | wc -l)
-Pronouns=$(grep -Eo ":\w+\s+PRON[0-9]*-*.*;" $lexcfile | grep -v "[<>]" | sort | uniq | wc -l)
+Adjectives=$(grep -Eo ":[[:alnum:][:space:]%-\{\}]+\s+A[0-9].*;" $lexcfile | grep -v "[<>]" | sort | uniq | wc -l)
+Verbs=$(grep -Eo ":[[:alnum:][:space:]%-\{\}]+\s+V-.*;" $lexcfile | grep -v "[<>]" | sort | uniq | wc -l)
+ProperNouns=$(grep -Eo ":[[:alnum:][:space:]%-\{\}]+\s+NP[0-9]*-.*;" $lexcfile | grep -v "[<>]" | sort | uniq | wc -l)
+Adverbs=$(grep -Eo ":[[:alnum:][:space:]%-\{\}]+\s+ADV[0-9]*-*.*;" $lexcfile | grep -v "[<>]" | sort | uniq | wc -l)
+Dets=$(grep -Eo ":[[:alnum:][:space:]%-\{\}]+\s+DET[0-9]*-*.*;" $lexcfile | grep -v "[<>]" | sort | uniq | wc -l)
+Conjunctions=$(grep -Eo ":[[:alnum:][:space:]%-\{\}]+\s+C[CAS].*;" $lexcfile | grep -v "[<>]" | sort | uniq | wc -l)
+Postpositions=$(grep -Eo ":[[:alnum:][:space:]%-\{\}]+\s+POST[0-9]*-*.*;" $lexcfile | grep -v "[<>]" | sort | uniq | wc -l)
+Ijs=$(grep -Eo ":[[:alnum:][:space:]%-\{\}]+\s+INTERJ[0-9]*-*.*;" $lexcfile | grep -v "[<>]" | sort | uniq | wc -l)
+Numerals=$(grep -Eo ":[[:alnum:][:space:]%-\{\}]+\s+NUM[0-9]*-*.*;" $lexcfile | grep -v "[<>]" | sort | uniq | wc -l)
+Pronouns=$(grep -Eo ":[[:alnum:][:space:]%-\{\}]+\s+PRON[0-9]*-*.*;" $lexcfile | grep -v "[<>]" | sort | uniq | wc -l)
 Other=$(echo "$totalcount-($Nouns+$Adjectives+$Verbs+$ProperNouns+$Adverbs+$Dets+$Conjunctions+$Postpositions+$Numerals+$Pronouns+$Ijs)" | calc -p)
 
 echo "$LG	$totalcount	$Nouns	$ProperNouns	$Verbs	$Adjectives	$Other";
